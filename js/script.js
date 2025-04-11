dbde2098fe238e710fde6b3f057a5f84864a2a2b
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-04-11 16:43:51 +0000

    added changes to make backgroun grey

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
+/* global Phaser */
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+
+console.log(game)
