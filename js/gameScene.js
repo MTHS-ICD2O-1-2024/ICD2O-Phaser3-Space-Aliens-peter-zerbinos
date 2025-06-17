022016de3475a212af41ed896ceb5fde357bc996
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-06-17 03:15:10 +0000

    missiles now have alien collision

--- js/gameScene.js
+++ js/gameScene.js
@@ -41,9 +41,10 @@ class GameScene extends Phaser.Scene {
     this.load.image('starBackground', './assets/starBackground.png')
     this.load.image('ship', './assets/spaceShip.png')
     this.load.image('missile', './assets/missile.png')
-    this.load.image('alien', 'assets/alien.png')
+    this.load.image('alien', './assets/alien.png')
     // sound
-    this.load.audio('laser', 'assets/laser1.wav')
+    this.load.audio('laser', './assets/laser1.wav')
+    this.load.audio('explosion', './assets/barrelExploding.wav')
   }
 
   create (data) {
@@ -58,6 +59,15 @@ class GameScene extends Phaser.Scene {
     // create a group for the aliens
     this.alienGroup = this.add.group()
     this.createAlien()
+
+    // Collisions between missiles and aliens
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play('explosion')
+      this.createAlien()
+      this.createAlien()
+    }.bind(this))
   }
 
   update (time, delta) {
