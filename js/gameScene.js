862de26572197e7fef332a5558798c52b0418327
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-06-17 02:43:14 +0000

    updated missles to move and play audio

--- js/gameScene.js
+++ js/gameScene.js
@@ -19,33 +19,21 @@ class GameScene extends Phaser.Scene {
     this.fireMissile = false
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * This method is called by the Scene Manager when the scene starts,
-   *  before preload () and create ().
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   init (data) {
     this.cameras.main.setBackgroundColor('#ffffff')
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to load assets.
-   */
   preload () {
     console.log('Game Scene')
 
+    // images
     this.load.image('starBackground', './assets/starBackground.png')
     this.load.image('ship', './assets/spaceShip.png')
     this.load.image('missile', './assets/missile.png')
+    // sound
+    this.load.audio('laser', 'assets/laser1.wav')
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to create your game objects.
-   * @param {object} data Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   create (data) {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
@@ -56,12 +44,6 @@ class GameScene extends Phaser.Scene {
     this.missileGroup = this.physics.add.group()
   }
 
-  /**
-   * Should be overridden by your own Scenes.
-   * This method is called once per game step while the scene is running.
-   * @param {number} time The current time.
-   * @param {number} delta The delta time in ms since the last frame.
-   */
   update (time, delta) {
     // called 60 times a second
 
@@ -89,12 +71,20 @@ class GameScene extends Phaser.Scene {
         this.fireMissile = true
         const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
         this.missileGroup.add(aNewMissile)
+        this.sound.play('laser')
       }
     }
 
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
     }
+
+    this.missileGroup.children.each(function (item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
   }
 }
 
